865c128c6ad34419d43654ee31cb9aa22f72972a
Dan Thornton <dan.thornton+Git@thornton.me>
2023-02-09 20:28:45 -0500

    reading device_id of imu (0x67 = 103u8) hardcoded

---
+++ esp32c3-hal/examples/i2c_device_id.rs
@@ -0,0 +1,62 @@
+//! Read Device ID register from ICM42670P sensor
+//!
+//! This example was copied from i2c_bmp180_calibration_data.rs
+//!
+//! The following wiring is assumed:
+//! - SDA => GPIO1
+//! - SCL => GPIO2
+
+#![no_std]
+#![no_main]
+
+use esp32c3_hal::{
+    clock::ClockControl,
+    gpio::IO,
+    i2c::I2C,
+    peripherals::Peripherals,
+    prelude::*,
+    timer::TimerGroup,
+    Rtc,
+};
+use esp_backtrace as _;
+use esp_println::println;
+use esp_riscv_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let mut wdt1 = timer_group1.wdt;
+
+    // Disable watchdog timers
+    rtc.swd.disable();
+    rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    // Create a new peripheral object with the described wiring
+    // and standard I2C clock speed
+    let mut i2c = I2C::new(
+        peripherals.I2C0,
+        io.pins.gpio10, //sda
+        io.pins.gpio8,  //scl
+        400u32.kHz(),
+        &mut system.peripheral_clock_control,
+        &clocks,
+    );
+
+    loop {
+        let mut data = [0u8; 22];
+        i2c.write_read(0b1101000, &[0x75], &mut data).ok();
+
+        println!("{:02x?}", data);
+    }
+}
